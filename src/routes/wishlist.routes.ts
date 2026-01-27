6eb614be675c6cfac12993b1822349ec38961419
FrancoDev <peschard.glenard@proton.me>
2026-01-27 10:51:04 +0100

    [FEAT] Resolved route

--- src/routes/wishlist.routes.ts
+++ src/routes/wishlist.routes.ts
@@ -1,195 +1,195 @@
-// import { Router } from "express";
-// import { Request, Response, NextFunction } from "express";
-// import ResponsesUtil from "@utils/responses.util";
-// import { AddGameWishlist, AddPlatformWishlist } from "@utils/interfaces/wishlist.interface";
-// import WishlistService from "@services/wishlist.service";
-// import { authMiddleware } from "@middlewares/auth.middleware";
+import { Router } from "express";
+import { Request, Response, NextFunction } from "express";
+import ResponsesUtil from "@utils/responses.util";
+import { AddGameWishlist, AddPlatformWishlist } from "@utils/interfaces/wishlist.interface";
+import WishlistService from "@services/wishlist.service";
+import { authMiddleware } from "@middlewares/auth.middleware";
 
-// const router: Router = Router();
+const router: Router = Router();
 
 
-// router.post('/add/game', authMiddleware, async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-// 	try {
+router.post('/add/game', authMiddleware, async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+	try {
 		
-// 		const addGameWishlist: AddGameWishlist = req.body.addGameWishlist || req.body;
-// 		if (!addGameWishlist.userId || !addGameWishlist.gameId || !addGameWishlist.platformId) {
-// 			return ResponsesUtil.invalidParameters(res, { error: 'MISSING_PARAMETERS' });
-// 		}
+		const addGameWishlist: AddGameWishlist = req.body.addGameWishlist || req.body;
+		if (!addGameWishlist.userId || !addGameWishlist.gameId || !addGameWishlist.platformId) {
+			return ResponsesUtil.invalidParameters(res, { error: 'MISSING_PARAMETERS' });
+		}
 
-// 		const result = await WishlistService.addGameToWishlist(
-// 			addGameWishlist.userId,
-// 			addGameWishlist.gameId,
-// 			addGameWishlist.platformId,
-// 			addGameWishlist.priceTarget,
-// 			addGameWishlist.notes || '',
-// 			addGameWishlist.addedAt,
-// 			addGameWishlist.priorityId || addGameWishlist.priority || '',
-// 			addGameWishlist.condition
-// 		);
-// 		if (!result) {
-// 			return ResponsesUtil.notFound(res, { error: 'ADD_GAME_TO_WISHLIST_FAILED' });
-// 		}
+		const result = await WishlistService.addGameToWishlist(
+			addGameWishlist.userId,
+			addGameWishlist.gameId,
+			addGameWishlist.platformId,
+			addGameWishlist.priceTarget,
+			addGameWishlist.notes || '',
+			addGameWishlist.addedAt,
+			addGameWishlist.priorityId || addGameWishlist.priority || '',
+			addGameWishlist.condition
+		);
+		if (!result) {
+			return ResponsesUtil.notFound(res, { error: 'ADD_GAME_TO_WISHLIST_FAILED' });
+		}
 
-// 		return ResponsesUtil.handleResult(res, { info: 'execok', data: { result } });
-// 	} catch (error) {
-// 		return ResponsesUtil.somethingWentWrong(res, { error: 'ADD_GAME_TO_WISHLIST_FAILED', details: error as string });
-// 	}
-// });
+		return ResponsesUtil.handleResult(res, { info: 'execok', data: { result } });
+	} catch (error) {
+		return ResponsesUtil.somethingWentWrong(res, { error: 'ADD_GAME_TO_WISHLIST_FAILED', details: error as string });
+	}
+});
 
-// router.post('/add/platform', authMiddleware, async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-// 	try {
+router.post('/add/platform', authMiddleware, async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+	try {
 		
-// 		const addPlatformWishlist: AddPlatformWishlist = req.body.addPlatformWishlist || req.body;
-// 		if (!addPlatformWishlist.userId || !addPlatformWishlist.platformId) {
-// 			return ResponsesUtil.invalidParameters(res, { error: 'MISSING_PARAMETERS' });
-// 		}
+		const addPlatformWishlist: AddPlatformWishlist = req.body.addPlatformWishlist || req.body;
+		if (!addPlatformWishlist.userId || !addPlatformWishlist.platformId) {
+			return ResponsesUtil.invalidParameters(res, { error: 'MISSING_PARAMETERS' });
+		}
 
-// 		const result = await WishlistService.addPlatformToWishlist(
-// 			addPlatformWishlist.userId,
-// 			addPlatformWishlist.platformId,
-// 			addPlatformWishlist.priceTarget,
-// 			addPlatformWishlist.notes || '',
-// 			addPlatformWishlist.addedAt,
-// 			addPlatformWishlist.priorityId || '',
-// 			addPlatformWishlist.condition || ''
-// 		);
-// 		if (!result) {
-// 			return ResponsesUtil.notFound(res, { error: 'ADD_PLATFORM_TO_WISHLIST_FAILED' });
-// 		}
+		const result = await WishlistService.addPlatformToWishlist(
+			addPlatformWishlist.userId,
+			addPlatformWishlist.platformId,
+			addPlatformWishlist.priceTarget,
+			addPlatformWishlist.notes || '',
+			addPlatformWishlist.addedAt,
+			addPlatformWishlist.priorityId || '',
+			addPlatformWishlist.condition || ''
+		);
+		if (!result) {
+			return ResponsesUtil.notFound(res, { error: 'ADD_PLATFORM_TO_WISHLIST_FAILED' });
+		}
 
-// 		return ResponsesUtil.handleResult(res, { info: 'execok', data: { result } });
-// 	} catch (error) {
-// 		return ResponsesUtil.somethingWentWrong(res, { error: 'ADD_PLATFORM_TO_WISHLIST_FAILED', details: error as string });
-// 	}
-// });
+		return ResponsesUtil.handleResult(res, { info: 'execok', data: { result } });
+	} catch (error) {
+		return ResponsesUtil.somethingWentWrong(res, { error: 'ADD_PLATFORM_TO_WISHLIST_FAILED', details: error as string });
+	}
+});
 
-// router.get('/:userId/platform/:platformId/games/list', authMiddleware, async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-// 	try {
-// 		const userId = req.params.userId;
-// 		const platformId = req.params.platformId;
-// 		if (!userId || !platformId) {
-// 			return ResponsesUtil.invalidParameters(res, { error: 'USER_ID_NOT_FOUND' });
-// 		}
+router.get('/:userId/platform/:platformId/games/list', authMiddleware, async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+	try {
+		const userId = req.params.userId;
+		const platformId = req.params.platformId;
+		if (!userId || !platformId) {
+			return ResponsesUtil.invalidParameters(res, { error: 'USER_ID_NOT_FOUND' });
+		}
 
-// 		const result = await WishlistService.getGamesFromWishlist(userId, platformId);
-// 		if (!result) {
-// 			return ResponsesUtil.notFound(res, { error: 'GET_GAMES_FROM_WISHLIST_FAILED' });
-// 		}
+		const result = await WishlistService.getGamesFromWishlist(userId, platformId);
+		if (!result) {
+			return ResponsesUtil.notFound(res, { error: 'GET_GAMES_FROM_WISHLIST_FAILED' });
+		}
 
-// 		return ResponsesUtil.handleResult(res, { info: 'execok', data: { result } });
-// 	} catch (error) {
-// 		return ResponsesUtil.somethingWentWrong(res, { error: 'GET_GAMES_FROM_WISHLIST_FAILED', details: error as string });
-// 	}
-// });
-
-
-// router.post('/game/delete', authMiddleware, async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-// 	try {
-// 		const { gameId, userId, platformId } = req.body;
-// 		if (!gameId || !userId || !platformId) {
-// 			return ResponsesUtil.invalidParameters(res, { error: 'MISSING_PARAMETERS' });
-// 		}
-
-// 		const result = await WishlistService.deleteGameFromWishlist(userId, gameId, platformId);
-// 		if (!result) {
-// 			return ResponsesUtil.notFound(res, { error: 'DELETE_GAME_FROM_WISHLIST_FAILED' });
-// 		}
-
-// 		return ResponsesUtil.handleResult(res, { info: 'execok', data: { result } });
-// 	} catch (error) {
-// 		return ResponsesUtil.somethingWentWrong(res, { error: 'DELETE_GAME_FROM_WISHLIST_FAILED', details: error as string });
-// 	}
-// });
+		return ResponsesUtil.handleResult(res, { info: 'execok', data: { result } });
+	} catch (error) {
+		return ResponsesUtil.somethingWentWrong(res, { error: 'GET_GAMES_FROM_WISHLIST_FAILED', details: error as string });
+	}
+});
 
 
-// router.get('/:userId/platforms/list', authMiddleware, async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-// 	try {
-// 		const userId = req.params.userId;
-// 		if (!userId) {
-// 			return ResponsesUtil.invalidParameters(res, { error: 'USER_ID_NOT_FOUND' });
-// 		}
-// 		const result = await WishlistService.getWishlistPlatforms(userId);
-// 		if (!result) {
-// 			return ResponsesUtil.notFound(res, { error: 'GET_WISHLIST_PLATFORMS_FAILED' });
-// 		}
-// 		return ResponsesUtil.handleResult(res, { info: 'execok', data: { result } });
-// 	} catch (error) {
-// 		return ResponsesUtil.somethingWentWrong(res, { error: 'GET_GAMES_FROM_WISHLIST_BY_PLATFORM_FAILED', details: error as string });
-// 	}	
-// });
+router.post('/game/delete', authMiddleware, async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+	try {
+		const { gameId, userId, platformId } = req.body;
+		if (!gameId || !userId || !platformId) {
+			return ResponsesUtil.invalidParameters(res, { error: 'MISSING_PARAMETERS' });
+		}
 
-// router.get('/:userId/platform/:platformId/game/:gameId', authMiddleware, async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-// 	try {
-// 		const userId = req.params.userId;
-// 		const gameId = req.params.gameId;
-// 		const platformId = req.params.platformId;
-// 		if (!userId || !gameId || !platformId) {
-// 			return ResponsesUtil.invalidParameters(res, { error: 'MISSING_PARAMETERS' });
-// 		}
-// 		const result = await WishlistService.getWishlistPlatformGame(userId, gameId, platformId);
-// 		if (!result) {
-// 			return ResponsesUtil.notFound(res, { error: 'GET_GAMES_FROM_WISHLIST_BY_PLATFORM_FAILED' });
-// 		}
-// 		return ResponsesUtil.handleResult(res, { info: 'execok', data: { result } });
-// 	}
-// 	catch (error) {
-// 		return ResponsesUtil.somethingWentWrong(res, { error: 'GET_GAMES_FROM_WISHLIST_BY_PLATFORM_FAILED', details: error as string });
-// 	}
-// });
+		const result = await WishlistService.deleteGameFromWishlist(userId, gameId, platformId);
+		if (!result) {
+			return ResponsesUtil.notFound(res, { error: 'DELETE_GAME_FROM_WISHLIST_FAILED' });
+		}
 
-// router.get('/priorities', authMiddleware, async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-// 	try {
-// 		const result = await WishlistService.getPriorities();
-// 		if (!result) {
-// 			return ResponsesUtil.notFound(res, { error: 'GET_PRIORITIES_FAILED' });
-// 		}
-// 		return ResponsesUtil.handleResult(res, { info: 'execok', data: { result } });
-// 	} catch (error) {
-// 		return ResponsesUtil.somethingWentWrong(res, { error: 'GET_PRIORITIES_FAILED', details: error as string });
-// 	}
-// });
+		return ResponsesUtil.handleResult(res, { info: 'execok', data: { result } });
+	} catch (error) {
+		return ResponsesUtil.somethingWentWrong(res, { error: 'DELETE_GAME_FROM_WISHLIST_FAILED', details: error as string });
+	}
+});
 
-// router.get('/:userId/games/list', authMiddleware, async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-// 	try {
-// 		const userId = req.params.userId;
-// 		if (!userId) {
-// 			return ResponsesUtil.invalidParameters(res, { error: 'MISSING_PARAMETERS' });
-// 		}
-// 		const result = await WishlistService.getGamesList(userId);
-// 		if (!result) {
-// 			return ResponsesUtil.notFound(res, { error: 'GAMES_LIST_NOT_FOUND' });
-// 		}
-// 		return ResponsesUtil.handleResult(res, { info: 'execok', data: { result } });
-// 	} catch (error) {
-// 		return ResponsesUtil.somethingWentWrong(res, { id_case: 'GET_GAMES_LIST_FAILED', error: error });
-// 	}
-// });
 
-// router.get('/:userId/platforms/wish', authMiddleware, async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-// 	try {
-// 		const userId = req.params.userId;
-// 		if (!userId) {
-// 			return ResponsesUtil.invalidParameters(res, { error: 'MISSING_PARAMETERS' });
-// 		}
-// 		const result = await WishlistService.getPlatformsFromWishlist(userId);
-// 		if (!result) {
-// 			return ResponsesUtil.notFound(res, { error: 'GET_WISHLIST_PLATFORMS_FAILED' });
-// 		}
-// 		return ResponsesUtil.handleResult(res, { info: 'execok', data: { result } });
-// 	} catch (error) {
-// 		return ResponsesUtil.somethingWentWrong(res, { error: 'GET_WISHLIST_PLATFORMS_FAILED', details: error as string });
-// 	}
-// });
+router.get('/:userId/platforms/list', authMiddleware, async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+	try {
+		const userId = req.params.userId;
+		if (!userId) {
+			return ResponsesUtil.invalidParameters(res, { error: 'USER_ID_NOT_FOUND' });
+		}
+		const result = await WishlistService.getWishlistPlatforms(userId);
+		if (!result) {
+			return ResponsesUtil.notFound(res, { error: 'GET_WISHLIST_PLATFORMS_FAILED' });
+		}
+		return ResponsesUtil.handleResult(res, { info: 'execok', data: { result } });
+	} catch (error) {
+		return ResponsesUtil.somethingWentWrong(res, { error: 'GET_GAMES_FROM_WISHLIST_BY_PLATFORM_FAILED', details: error as string });
+	}	
+});
 
-// /***************************************************************
-// * NOT ALLOWED METHODS HANDLING
-// ***************************************************************/
+router.get('/:userId/platform/:platformId/game/:gameId', authMiddleware, async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+	try {
+		const userId = req.params.userId;
+		const gameId = req.params.gameId;
+		const platformId = req.params.platformId;
+		if (!userId || !gameId || !platformId) {
+			return ResponsesUtil.invalidParameters(res, { error: 'MISSING_PARAMETERS' });
+		}
+		const result = await WishlistService.getWishlistPlatformGame(userId, gameId, platformId);
+		if (!result) {
+			return ResponsesUtil.notFound(res, { error: 'GET_GAMES_FROM_WISHLIST_BY_PLATFORM_FAILED' });
+		}
+		return ResponsesUtil.handleResult(res, { info: 'execok', data: { result } });
+	}
+	catch (error) {
+		return ResponsesUtil.somethingWentWrong(res, { error: 'GET_GAMES_FROM_WISHLIST_BY_PLATFORM_FAILED', details: error as string });
+	}
+});
 
-// router.all('/actives', async (req: Request, res: Response, next: NextFunction): Promise<void> => ResponsesUtil.methodNotAllowed(res));
+router.get('/priorities', authMiddleware, async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+	try {
+		const result = await WishlistService.getPriorities();
+		if (!result) {
+			return ResponsesUtil.notFound(res, { error: 'GET_PRIORITIES_FAILED' });
+		}
+		return ResponsesUtil.handleResult(res, { info: 'execok', data: { result } });
+	} catch (error) {
+		return ResponsesUtil.somethingWentWrong(res, { error: 'GET_PRIORITIES_FAILED', details: error as string });
+	}
+});
 
-// /**************************************************************/
+router.get('/:userId/games/list', authMiddleware, async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+	try {
+		const userId = req.params.userId;
+		if (!userId) {
+			return ResponsesUtil.invalidParameters(res, { error: 'MISSING_PARAMETERS' });
+		}
+		const result = await WishlistService.getGamesList(userId);
+		if (!result) {
+			return ResponsesUtil.notFound(res, { error: 'GAMES_LIST_NOT_FOUND' });
+		}
+		return ResponsesUtil.handleResult(res, { info: 'execok', data: { result } });
+	} catch (error) {
+		return ResponsesUtil.somethingWentWrong(res, { id_case: 'GET_GAMES_LIST_FAILED', error: error });
+	}
+});
 
-// /*
-// 	Export
-// */
-// export { router as WishlistRouter };
+router.get('/:userId/platforms/wish', authMiddleware, async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+	try {
+		const userId = req.params.userId;
+		if (!userId) {
+			return ResponsesUtil.invalidParameters(res, { error: 'MISSING_PARAMETERS' });
+		}
+		const result = await WishlistService.getPlatformsFromWishlist(userId);
+		if (!result) {
+			return ResponsesUtil.notFound(res, { error: 'GET_WISHLIST_PLATFORMS_FAILED' });
+		}
+		return ResponsesUtil.handleResult(res, { info: 'execok', data: { result } });
+	} catch (error) {
+		return ResponsesUtil.somethingWentWrong(res, { error: 'GET_WISHLIST_PLATFORMS_FAILED', details: error as string });
+	}
+});
+
+/***************************************************************
+* NOT ALLOWED METHODS HANDLING
+***************************************************************/
+
+router.all('/actives', async (req: Request, res: Response, next: NextFunction): Promise<void> => ResponsesUtil.methodNotAllowed(res));
+
+/**************************************************************/
+
+/*
+	Export
+*/
+export { router as WishlistRouter };
